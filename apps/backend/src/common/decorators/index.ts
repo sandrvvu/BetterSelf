3fe21d6b653be1a44a31d03d1c0f49898e14c139
sandrvvu <sandrvvu@gmail.com>
2025-02-14 16:45:10 +0200

    feat: edit user name and password

--- apps/backend/src/common/decorators/index.ts
+++ apps/backend/src/common/decorators/index.ts
@@ -1,1 +1,2 @@
 export { IS_PUBLIC_KEY, Public } from "./public.decorator";
+export { CurrentUserId } from "./current-user-id.decorator";
--- apps/backend/src/modules/users/user.controller.ts
+++ apps/backend/src/modules/users/user.controller.ts
@@ -1,10 +1,49 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Put,
+  ValidationPipe,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
 
+import { CurrentUserId } from "src/common/decorators";
+
+import { UpdateUserDto } from "./libs/dto/update-user.dto";
+import { User } from "./user.entity";
 import { UserService } from "./user.service";
 
 @ApiTags("Users")
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth("access-token")
+  @ApiBody({
+    type: UpdateUserDto,
+  })
+  @ApiOkResponse({ type: User })
+  @ApiResponse({
+    description: "The user data has been successfully updated.",
+    status: 200,
+  })
+  @ApiResponse({ description: "Unauthorized.", status: 401 })
+  @ApiResponse({ description: "User is not found.", status: 404 })
+  @ApiResponse({ description: "Invalid data type provided.", status: 400 })
+  async updateUser(
+    @CurrentUserId() id: string,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
 }
