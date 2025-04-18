a1b7448ffdaea7565edd108e21692af34191258f
sandrvvu <sandrvvu@gmail.com>
2025-04-18 14:53:29 +0300

    feat: adding intro page

---
+++ apps/frontend/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-indigo-800 via-violet-400 to-neutral text-white/90 flex flex-col">
+      <div className="flex-grow flex flex-col justify-center items-center text-center mx-6 px-5">
+        <h1 className="text-[7rem] md:text-[9rem] font-gravitas font-extrabold mb-4 drop-shadow-lg">
+          404
+        </h1>
+        <p className="text-xl md:text-6xl font-semibold">
+        Oops! Nothing Found Here
+        </p>
+        <p className="text-sm md:text-2xl">
+          We&apos;re sorry, but the page you were looking for could not be
+          found.
+        </p>
+        <Link href="/">
+            <button className="border-2 border-white text-md md:text-2xl my-10 px-4 py-2 rounded-xl hover:bg-white/90 hover:text-indigo-800 transition">
+            Go back to the homepage
+            </button>
+          </Link>
+      </div>
+    </main>
+  );
+}
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,100 +1,33 @@
-import Image, { type ImageProps } from "next/image";
-// import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Link from "next/link";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
+export default function Intro() {
   return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/frontend/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turbo.build/repo/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className="min-h-screen bg-gradient-to-br from-indigo-800 via-violet-400 to-neutral text-white flex flex-col">
+      <header className="w-full flex justify-between items-center p-6">
+        <div className="text-xl sm:text-2xl font-bold">BetterSelf</div>
+        <div className="space-x-4">
+          <Link href="/sign-in">
+            <button className="bg-white text-indigo-600 px-4 py-2 rounded-xl font-semibold shadow hover:bg-gray-100 transition">
+              Sign In
+            </button>
+          </Link>
+          <Link href="/login">
+            <button className="border border-white px-4 py-2 rounded-xl font-semibold hover:bg-white hover:text-indigo-600 transition">
+              Log In
+            </button>
+          </Link>
         </div>
-        <button className={styles.secondary}>Open alert</button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turbo.build?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turbo.build →
-        </a>
-      </footer>
-    </div>
+      </header>
+
+      <div className="flex-grow flex flex-col justify-center items-center text-center px-4 mx-5">
+        <h1 className="text-5xl md:text-6xl lg:text-8xl font-gravitas font-extrabold mb-4 drop-shadow-lg">
+          Rise. Grow. Achieve
+        </h1>
+        <p className="text-2xl md:text-3xl text-white/90">
+          Your best life isn&apos;t a dream — it&apos;s a plan. Let&apos;s make
+          it happen, together
+        </p>
+      </div>
+    </main>
   );
 }
